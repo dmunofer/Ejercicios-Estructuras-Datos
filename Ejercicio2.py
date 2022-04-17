16eb082dbcecf436d9325ea84dcf9a478db3647c
dmunofer <diego.fmz@hotmail.com>
2022-04-17 17:06:48 +0200

    clase controlador

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -4,3 +4,11 @@ class Vista():
 
         self.entrada=str(input('Primera entrada:'))
         self.entrada2=str(input('Segunda entrada:'))
+
+
+class Controlador():
+
+    def __init__(self, entrada, entrada2):
+
+        self.primera=str(entrada).upper()
+        self.segunda=str(entrada2).upper()
