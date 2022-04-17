5511bae346d12b52ffeb55e55073603a8be455f7
dmunofer <diego.fmz@hotmail.com>
2022-04-17 17:20:34 +0200

    clase naturaleza

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,4 +1,16 @@
 
+class Naturaleza():
+
+    ALIMENTARIA = 0.055
+    SERVICIO = 0.2
+
+class Producto(Naturaleza):
+
+    def __init__(self,):
+
+
+
+
 
 
 
@@ -12,7 +24,6 @@
 
 
 '''
-
 producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
 precio_neto = FactoryFactura.crear(producto).facturar() 
 print(precio_neto) 
@@ -22,5 +33,4 @@ producto = Producto(Naturaleza.SERVICIO) # IVA 20%
 precio_neto = FactoryFactura.crear(producto).facturar() 
 print(precio_neto) 
 # 120 
-
 '''
