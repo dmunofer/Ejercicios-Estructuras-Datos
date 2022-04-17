86fdea44ba6493a9c132edc11f8a17545d028ecd
dmunofer <diego.fmz@hotmail.com>
2022-04-17 17:35:54 +0200

    terminado y probado con ejemplo

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -9,35 +9,19 @@ class Producto(Naturaleza):
     def __init__(self,naturaleza):
         self.naturaleza=naturaleza
         self.precio =100
-    
+
     def facturar(self):
         return (100*self.naturaleza)+self.precio
 
+class FactoryFactura(Producto):
 
-        
-
-
-
-
-
+    def crear(self,producto):
+        self.producto = producto
+        return self.producto
 
 
 
-
-
-
-
-
-
-
-'''
-producto = Producto(Naturaleza.ALIMENTARIA) # IVA 5,5% 
-precio_neto = FactoryFactura.crear(producto).facturar() 
-print(precio_neto) 
-# 105.5 
- 
-producto = Producto(Naturaleza.SERVICIO) # IVA 20% 
-precio_neto = FactoryFactura.crear(producto).facturar() 
-print(precio_neto) 
-# 120 
-'''
+producto = Producto(Naturaleza.ALIMENTARIA)
+factura = FactoryFactura(Producto)
+precio_neto = factura.crear(producto).facturar()
+print(precio_neto)
